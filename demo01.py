078cb2b15498beecf9e97461d1f026bfcfce875d
YoRoll-cbn <2356823260@qq.com>
2021-05-09 12:28:40 +0800

    第一次向GitHub提交代码

---
+++ demo01.py
@@ -0,0 +1,21 @@
+'''
+print("hello word！") #字符串
+print(980702) #整数
+print(98.0702) #小数
+print(True) #布尔值
+print(()) #元组
+print([]) #数组
+print({}) #字典
+print("张颜齐",980702)
+print("张颜齐"+"林彦俊")
+print("张颜齐"*20)
+print(((1+2)*300-99)/2)
+a=input("请输入：")
+print("input获取的内容",a)
+print(type(980702),type(()),type([]),type({}))
+print(type(int("980702")))
+a=float(input("请输入："))
+b=float(input("请输入："))
+print("a+b=",a+b)
+'''
+print(len("1323546"))
