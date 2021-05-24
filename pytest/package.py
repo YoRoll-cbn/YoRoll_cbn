f1d37333af0f6912fbfde9c046097331a6ef2d6f
YoRoll-cbn <2356823260@qq.com>
2021-05-24 15:49:49 +0800

    allure

--- pytest/package.py
+++ pytest/package.py
@@ -3,4 +3,9 @@ import pytest
 from HTMLTestRunner import HTMLTestRunner
 
 if __name__ == '__main__':
-    pytest.main(['-vs'])
+    #-vs表示打印结果
+    #pytest.main('-vs')
+     #第一步：生成json格式临时文件，--clean-alluredir表示每执行一次把temp目录先清除
+    pytest.main(['-vs',r'D:\python\GitHub\YoRoll_cbn\pytest\test_login.py','--alluredir','./temp','--clean-alluredir'])
+    #第二步：根据json格式临时文件生成allure报告
+    os.system("allure generate ./pytest/temp -o ./reports --clean")
